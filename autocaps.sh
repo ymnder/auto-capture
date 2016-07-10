6b071f3f6fe5980e703c39f5af2b0c2efa7b33dd
yamazakiryo <yamazaki.waseda@gmail.com>
2016-07-10 13:47:56 +0900

    add some code

--- autocaps.sh
+++ autocaps.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# 画面サイズからswipeを行う
+adb shell wm size | grep -o -e "[0-9]\{2,\}" >> tmp
+# ホームに戻しておく
+adb shell input keyevent KEYCODE_HOME
 
 DATE=`date +"%Y-%m-%d-%H-%M-%S"`
 
@@ -15,18 +19,22 @@ adb shell rm "/sdcard/${FILENAME}"
 adb shell service call statusbar 2
 echo "saved ${FILENAME}."
 
+
 # 次に各アプリのtopを撮る
 # パッケージ名の取得
 # adb shell pm list packages
-PACKAGES=("com.nikkei.news.dev" "jp.co.yahoo.android.news")
-for PACKAGE in ${PACKAGE[@]}; do
+PACKAGES=("com.newspicks" "jp.co.yahoo.android.news")
+for PACKAGE in ${PACKAGES[@]}; do
 	echo "capturing ${PACKAGE}..."
 	APPFILENAME="app-${DATE}.png"
-	adb shell monkey -p ${PACKAGE} -c android.intent.category.LAUNCHER 1\n
+	adb shell monkey -p ${PACKAGE} -c android.intent.category.LAUNCHER 1
 	sleep 10s
 	# もしスライドさせる必要があれば適宜利用
-	# adb shell input touchscreen swipe 0 200 500 200
-	# adb shell input touchscreen swipe 500 200 0 200
+	# adb shell input touchscreen tap x y
+	# adb shell input touchscreen swipe x1 y1 x2 y2
+	# 例：
+	# adb shell input touchscreen swipe 0 200 500 200 # swipe left
+	# adb shell input touchscreen swipe 500 200 0 200 # swipe right
 	# sleep 5s
 	adb shell screencap -p "/sdcard/${APPFILENAME}"
 	adb pull /sdcard/"${APPFILENAME}" ./autocap/${PACKAGE}/"${APPFILENAME}"
