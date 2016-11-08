e070644a8730f7c87ebb8ccfe8f39bf13ec58041
ymnder <yamazaki.waseda@gmail.com>
2016-11-08 15:38:48 +0900

    環境にあわせて修正

--- autocaps.sh
+++ autocaps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 画面サイズからswipeを行う
-adb shell wm size | grep -o -e "[0-9]\{2,\}" >> tmp
+# adb shell wm size | grep -o -e "[0-9]\{2,\}" >> tmp
 # ホームに戻しておく
 adb shell input keyevent KEYCODE_HOME
 
@@ -13,7 +13,7 @@ echo "capturing ${FILENAME}..."
 adb shell service call statusbar 1
 sleep 5s
 adb shell screencap -p "/sdcard/${FILENAME}"
-adb pull /sdcard/"${FILENAME}" ./autocap/stbar/"${FILENAME}"
+adb pull /sdcard/"${FILENAME}" ./autocap/not/"${FILENAME}"
 adb shell rm "/sdcard/${FILENAME}"
 # Collapse status bar
 adb shell service call statusbar 2
@@ -23,10 +23,10 @@ echo "saved ${FILENAME}."
 # 次に各アプリのtopを撮る
 # パッケージ名の取得
 # adb shell pm list packages
-PACKAGES=("com.newspicks" "jp.co.yahoo.android.news")
+PACKAGES=("jp.co.yahoo.android.news" "jp.gocro.smartnews.android" "jp.naver.linenews.android" "com.newspicks" "jp.www47news.Viewer" "jp.co.yahoo.android.yjtop" "com.gunosy.android")
 for PACKAGE in ${PACKAGES[@]}; do
 	echo "capturing ${PACKAGE}..."
-	APPFILENAME="app-${DATE}.png"
+	APPFILENAME="app-${PACKAGE}-${DATE}.png"
 	adb shell monkey -p ${PACKAGE} -c android.intent.category.LAUNCHER 1
 	sleep 10s
 	# もしスライドさせる必要があれば適宜利用
@@ -39,7 +39,7 @@ for PACKAGE in ${PACKAGES[@]}; do
 	adb shell input touchscreen swipe 200 400 200 2000 1000
 	sleep 10s
 	adb shell screencap -p "/sdcard/${APPFILENAME}"
-	adb pull /sdcard/"${APPFILENAME}" ./autocap/${PACKAGE}/"${APPFILENAME}"
+	adb pull /sdcard/"${APPFILENAME}" ./autocap/apps/"${APPFILENAME}"
 	adb shell rm "/sdcard/${APPFILENAME}"
 	# ホームに戻しておく
 	adb shell input keyevent KEYCODE_HOME
